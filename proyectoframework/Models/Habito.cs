e00f7d5766486079598ea26da02c0dddca97686d
Samircitoxd28 <kleber.mosquera@outlook.com>
2025-02-22 17:35:45 -0500

    Agregar archivos de proyecto.

---
+++ proyectoframework/Models/Habito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace proyectoframework.Models;
+
+public partial class Habito
+{
+    public int IdHabito { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+
+    public string? Duracion { get; set; }
+
+    public string? Tipo { get; set; }
+
+    public string? Color { get; set; }
+
+    public DateTime? FechaCreacion { get; set; }
+
+    public string? Estado { get; set; }
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+
+    public virtual ICollection<RegistroHabito> RegistroHabitos { get; set; } = new List<RegistroHabito>();
+}
