ea065e7130f828712d61040e98135805f2a78858
Samircitoxd28 <kleber.mosquera@outlook.com>
2025-02-22 19:28:32 -0500

    actualizacion, coneccion estadisticas

--- proyectoframework/Program.cs
+++ proyectoframework/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using proyectoframework;
 using proyectoframework.Components;
+using proyectoframework.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
+builder.Services.AddDbContext<HabitosDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
